ca705c1f0aad906177ec39e7e01078161c447c6b
kotohata <kotohata@attracie.com>
2015-04-18 11:45:06 +0900

    Add login page url

---
+++ app/controllers/front/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Front::SessionsController < FrontController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :front, path: '' do
     get 'join' => 'registrations#new'
     resource :registrations, only: [:create]
+    get 'login' => 'sessions#new'
+    resource :sessions, only: [:create, :destroy]
     root to: 'pages#top'
   end
 
