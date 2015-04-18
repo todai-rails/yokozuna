ef1579f27af50828d066f333f2aa6e06e942228e
kotohata <kotohata@attracie.com>
2015-04-18 11:26:21 +0900

    Create user

--- app/controllers/front/registrations_controller.rb
+++ app/controllers/front/registrations_controller.rb
@@ -6,5 +6,15 @@ class Front::RegistrationsController < FrontController
   def create
     # 送信先
     puts '送信先！！！'
+
+    # paramsのuserの中のnameとpasswordだけを許可します
+    @user = User.new(params.require(:user).permit(:name, :password))
+
+    # model.save はsaveできたらtrue, できなかったらfalseを返す
+    if @user.save
+      redirect_to front_root_url
+    else
+      render :new, status: 422
+    end
   end
 end
