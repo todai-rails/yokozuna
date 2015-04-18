1b07488bd6a6b648298574c66b5273011f8932a7
kotohata <kotohata@attracie.com>
2015-04-18 12:00:17 +0900

    Add login method

--- app/controllers/front/sessions_controller.rb
+++ app/controllers/front/sessions_controller.rb
@@ -4,7 +4,15 @@ class Front::SessionsController < FrontController
   end
 
   def create
+    user = User.find_by(name: params[:name])
 
+    if user && user.authenticate(params[:password])
+      # ログインできる
+      session[:user_id] = user.id
+      redirect_to front_login_url
+    else
+      redirect_to front_login_url
+    end
   end
 
   def destroy
